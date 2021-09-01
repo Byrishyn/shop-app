50be0b06f3f932932400046da7d9bc601f774160
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-09-01 17:23:32 +0900

    Save orders to server

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,5 +1,22 @@
 export const ADD_ORDER = "ADD_ORDER"
 
 export const addOrder = (cartItems, totalAmount) => {
-    return { type: ADD_ORDER, orderData: { items: cartItems, amount: totalAmount } }
+    return async dispatch => {
+        const date = new Date()
+        const response = await fetch("https://shop-app-cc5f7-default-rtdb.asia-southeast1.firebasedatabase.app/orders/u1.json", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                cartItems,
+                totalAmount,
+                date: date.toISOString()
+            })
+        })
+
+        const respData = await response.json()
+
+        dispatch({ type: ADD_ORDER, orderData: {id:respData.name, items: cartItems, amount: totalAmount, date: date } })
+    }
 }
