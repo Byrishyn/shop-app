7b7b0467eb05a10daf1fd3c99218b613d6f1d350
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-09-01 18:12:20 +0900

    Add orders to server

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,23 +1,29 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { FlatList, Platform, Text } from "react-native"
 import { HeaderButtons, Item } from "react-navigation-header-buttons"
 import HeaderButton from "../../components/UI/HeaderButton"
 import OrderItem from "../../components/shop/OrderItem"
-
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import * as ordersActions from "../../store/actions/orders"
 
 const OrdersScreen = props => {
     const orders = useSelector(state => state.orders.orders)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(ordersActions.fetchOrders())
+    }, [dispatch])
+
     return (
         <FlatList
             data={orders}
             keyExtractor={item => item.id}
             renderItem={itemData => (
-            <OrderItem 
-            amount={itemData.item.totalAmount} 
-            date={itemData.item.readableDate}
-            items={itemData.item.items}
-            />
+                <OrderItem
+                    amount={itemData.item.totalAmount}
+                    date={itemData.item.readableDate}
+                    items={itemData.item.items}
+                />
             )}
         />
     )
@@ -25,7 +31,7 @@ const OrdersScreen = props => {
 
 OrdersScreen.navigationOptions = navData => {
     return {
-        headerTitle : "Your Orders",
+        headerTitle: "Your Orders",
         headerLeft:
             <HeaderButtons HeaderButtonComponent={HeaderButton}>
                 <Item
--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,36 @@
+import Order from "../../models/order"
+
 export const ADD_ORDER = "ADD_ORDER"
+export const SET_ORDERS = "SET_ORDERS"
+
+export const fetchOrders = () => {
+    return async dispatch => {
+        try {
+            const response = await fetch("https://shop-app-cc5f7-default-rtdb.asia-southeast1.firebasedatabase.app/orders/u1.json")
+
+            if (!response.ok) {
+                throw new Error("Something went wrong !")
+            }
+
+            const respData = await response.json()
+            const loadedOrders = []
+
+            for (const key in respData) {
+                loadedOrders.push(
+                    new Order(
+                        key,
+                        respData[key].cartItems,
+                        respData[key].totalAmount,
+                        new Date(respData[key].date)
+                    )
+                )
+            }
+            dispatch({ type: SET_ORDERS, orders: loadedOrders });
+        } catch (err) {
+            throw err;
+        }
+    }
+}
 
 export const addOrder = (cartItems, totalAmount) => {
     return async dispatch => {
@@ -17,6 +49,6 @@ export const addOrder = (cartItems, totalAmount) => {
 
         const respData = await response.json()
 
-        dispatch({ type: ADD_ORDER, orderData: {id:respData.name, items: cartItems, amount: totalAmount, date: date } })
+        dispatch({ type: ADD_ORDER, orderData: { id: respData.name, items: cartItems, totalAmount, date: date } })
     }
 }
